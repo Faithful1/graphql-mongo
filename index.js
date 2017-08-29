d53f09d5cffeffa6e8b6d9e3fbfa7fc2300f1771
faithful <faithfulanere@gmail.com>
2017-08-29 16:22:39 +0800

    Mongoose Model Setup

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import graphqlHTTP from 'express-graphql';
 
 const app = express();
 
+//setup mongoose connection
+mongoose.connect('mongodb://Faithful:password@ds161493.mlab.com:61493/ffgraphql-api');
+const db = mongoose.connection;
+db.on('error', () => console.log('For some reason i have failed to connect to database, please check'))
+  .once('open', () => console.log('I am now Connected to DB'))
+
 //route router with some text
 app.get('/', (req, res) => {
   res.send('Hello Guys, this is graphql api by Faith.');
