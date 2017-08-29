89b99a1ffbe8c4b5c7ff5a0f818073cd64d4069f
faithful <faithfulanere@gmail.com>
2017-08-29 17:26:07 +0800

    add graphql Types

---
+++ graphql/types/post.js
@@ -0,0 +1,48 @@
+import {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLList
+} from 'graphql';
+
+import PostModel from '../../models/post';
+import { postType } deom './post';
+
+export const postType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'User api',
+  fields: () => ({
+  _id: {
+    type: new GraphQLNonNull(GraphQLID)
+  },
+  uid: {
+    type: GraphQLString
+  },
+  title: {
+    type: GraphQLString
+  },
+  body: {
+    type: GraphQLString
+    }
+  })
+});
+
+//create user input type which is useful for inserting post(graphql input)
+export const postInputType: new GraphQLInputObjectType({
+  name: 'PostInput',
+  description: 'Insert post',
+  fields: () => ({
+    uid: {
+      type: GraphQLString
+    },
+    title: {
+      type: GraphQLString
+    },
+    body: {
+      type: GraphQLString
+    }
+  })
+});
